4e5bbb69b4ad935301adfa3c465cdca96427ab13
Christian Lozano <christianlozano729@gmail.com>
2022-10-21 21:29:15 -0500

    login button + page added

--- crunchyroll-app/flask_app/controllers/users.py
+++ crunchyroll-app/flask_app/controllers/users.py
@@ -1,6 +1,8 @@
 from flask_app import app
-
-from flask import render_template, redirect
+import re
+from flask_bcrypt import Bcrypt
+bcrypt = Bcrypt(app)
+from flask import render_template, redirect, session, request, flash
 
 from flask_app.models.user import User
 
@@ -8,3 +10,39 @@ from flask_app.models.user import User
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/register', methods=['POST'])
+def register():
+    if not User.validate_user(request.form):
+        return redirect('/register')
+    data ={
+        'first_name': request.form['first_name'],
+        'last_name': request.form['last_name'],
+        'email': request.form['email'],
+        'password': bcrypt.generate_password_hash(request.form['password'])
+    }
+    id = User.Save(data)
+    session['user_id'] = id
+    return redirect('/dashboard')
+
+@app.route('/login', methods=['POST'])
+def login():
+    user= User.get_by_email(request.form)
+
+    if not user:
+        flash("Invalid Email", "login")
+        return redirect('/')
+    if not bcrypt.check_password_hash(user.password, request.form['password']):
+        flash("Invalid Password", "login")
+        return redirect('/')
+    session['user_id'] = user.id
+    return redirect('/dashboard')
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect("/")
+
+@app.route('/logpage')
+def logpage():
+    return render_template('login.html')
--- crunchyroll-app/flask_app/models/user.py
+++ crunchyroll-app/flask_app/models/user.py
@@ -1,4 +1,7 @@
 from flask_app.config.mysqlconnection import connectToMySQL
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0._-]+\.[a-zA-Z]+$')
+from flask import flash
 
 class User:
     def __init__( self , data ):
@@ -14,7 +17,7 @@ class User:
     @classmethod
     def get_all(cls):
         query = "SELECT * FROM users;"
-        results = connectToMySQL('users_CRUD').query_db(query)
+        results = connectToMySQL('anime').query_db(query)
 
         users = []
         
@@ -22,3 +25,38 @@ class User:
         for user in results:
             users.append( cls(user) )
         return users
+
+    @classmethod
+    def Save(cls):
+        query = "INSERT INTO users (first_name, last_name, email, password,) VALUES ( %(first_name)s , %(last_name)s, %(email)s, %(password)s);"
+        results = connectToMySQL('anime').query_db(query)
+        print(results)
+        return results
+
+
+    @staticmethod
+    def validate_user(user):
+        is_valid = True 
+        query = "SELECT * FROM users WHERE email = %(email)s;"
+        result= connectToMySQL('anime').query_db(query, user)
+        
+        if len(result) >= 1:
+            flash("Email already taken.", "register")
+            is_valid=False
+        if not EMAIL_REGEX.match(user['email']): 
+            flash("Invalid email address!", "register")
+            is_valid = False
+        if len(user['first_name']) < 2:
+            flash("First name must be at least 2 characters", "register")
+            is_valid= False
+        if len(user['last_name']) < 2:
+            flash("Last name must be at least 2 characters", "register")
+            is_valid= False
+        if len(user['password']) < 8:
+            flash("Password must be at least 8 characters", "register")
+            is_valid= False
+        if user['password'] != user['confirm']:
+            flash("Passwords don't match", "register")
+            is_valid= False
+
+        return is_valid
